9eb8a66cc502ffbc6c2d95ed3cd9823e4a054730
Min Kim <kmin638123@gmail.com>
2023-05-08 16:48:16 +0900

    adding goal graphs

--- component/todaygoal.py
+++ component/todaygoal.py
@@ -5,6 +5,8 @@ from dash.dependencies import Input, Output, State
 from dash import html, dcc
 from inputdata.goalsettingdata import usage_time_info, unlock_info, app_usage_info, is_goal_setted
 from component.goaldonutplot import goal_donut_plot, week_donut_plot
+from component.graph import usage_graph, unlock_graph, app_usage_graph
+
 
 def unlock_component(highlighted=None):
     data = 4
@@ -123,14 +125,8 @@ def unlock_weekly_calender(highlighted=None):
             html.Div(['Your Weekly - Unlock', table],
                      className='weekly-calander-container',),
             html.Div(children=[
-                html.P('Unlock')
-                # 여기다가 추가하면 됨
-                # 여기다가 추가하면 됨
-                # 여기다가 추가하면 됨
-                # 여기다가 추가하면 됨
-                # 여기다가 추가하면 됨
-                # 여기다가 추가하면 됨
-                # 여기다가 추가하면 됨
+                html.P('Unlock'),
+                html.Div(dcc.Graph(figure = unlock_graph(), config={'displayModeBar': False}),style={'margin-top':'-50px'})
             ])
         ])
     ]
@@ -167,14 +163,8 @@ def usage_weekly_calender(highlighted=None):
             html.Div(['Your Weekly - Usage Time', table],
                      className='weekly-calander-container',),
             html.Div(children=[
-                html.P('Usage Time')
-                # 여기다가 추가하면 됨
-                # 여기다가 추가하면 됨
-                # 여기다가 추가하면 됨
-                # 여기다가 추가하면 됨
-                # 여기다가 추가하면 됨
-                # 여기다가 추가하면 됨
-                # 여기다가 추가하면 됨
+                html.P('Usage Time'),
+                html.Div(dcc.Graph(figure = usage_graph(), config={'displayModeBar': False})),
             ])
         ])
     ]
@@ -211,14 +201,9 @@ def app_weekly_calender(highlighted=None):
             html.Div(['Your Weekly - App Usage', table],
                      className='weekly-calander-container',),
             html.Div(children=[
-                html.P('App Usage')
-                # 여기다가 추가하면 됨
-                # 여기다가 추가하면 됨
-                # 여기다가 추가하면 됨
-                # 여기다가 추가하면 됨
-                # 여기다가 추가하면 됨
-                # 여기다가 추가하면 됨
-                # 여기다가 추가하면 됨
+                html.P('App Usage'),
+                html.Div(dcc.Graph(figure = app_usage_graph(), config={'displayModeBar': False})),
+
             ])
         ])
     ]
